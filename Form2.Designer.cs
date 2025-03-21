964687dd8b1a4b7c3e7350e259860f6304f4fa87
57Anonymous34 <A.Jashari.inf24@stud.bbbaden.ch>
2025-03-21 11:30:27 +0100

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SpielParadies
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            startButton = new Button();
+            picCanvas = new PictureBox();
+            txtScore = new Label();
+            txtHighScore = new Label();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)picCanvas).BeginInit();
+            SuspendLayout();
+            // 
+            // startButton
+            // 
+            startButton.Location = new Point(598, 40);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(119, 84);
+            startButton.TabIndex = 0;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += StartGame;
+            // 
+            // picCanvas
+            // 
+            picCanvas.BackColor = SystemColors.ControlDark;
+            picCanvas.Location = new Point(1, 13);
+            picCanvas.Name = "picCanvas";
+            picCanvas.Size = new Size(580, 680);
+            picCanvas.TabIndex = 1;
+            picCanvas.TabStop = false;
+            picCanvas.Paint += UpdatePictureBoxGraphics;
+            // 
+            // txtScore
+            // 
+            txtScore.AutoSize = true;
+            txtScore.Location = new Point(604, 181);
+            txtScore.Name = "txtScore";
+            txtScore.Size = new Size(98, 32);
+            txtScore.TabIndex = 2;
+            txtScore.Text = "Score: 0";
+            // 
+            // txtHighScore
+            // 
+            txtHighScore.AutoSize = true;
+            txtHighScore.Location = new Point(604, 236);
+            txtHighScore.Name = "txtHighScore";
+            txtHighScore.Size = new Size(131, 32);
+            txtHighScore.TabIndex = 3;
+            txtHighScore.Text = "High Score";
+            // 
+            // gameTimer
+            // 
+            gameTimer.Interval = 40;
+            gameTimer.Tick += GameTimerEvent;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(585, 635);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 4;
+            button1.Text = "Zurück";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(738, 693);
+            Controls.Add(button1);
+            Controls.Add(txtHighScore);
+            Controls.Add(txtScore);
+            Controls.Add(picCanvas);
+            Controls.Add(startButton);
+            Name = "Form2";
+            Text = "Snake_Game";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)picCanvas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button startButton;
+        private PictureBox picCanvas;
+        private Label txtScore;
+        private Label txtHighScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private Button button1;
+    }
+}
